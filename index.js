0501ceffdcd2a1f8fec07d470fdb04ad3a39a561
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2023-10-24 21:32:29 +0530

    installed express, setup the server installed nodemon and body-parser

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+
+
+
+
+
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log(`An Error has occured in runing the server ${err}!`);
+        return;
+    }
+    return console.log(`The express server is up and running on port : ${port}`);
+})
